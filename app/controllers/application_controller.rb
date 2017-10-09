1929caab9be209dd72df7d1b8aa0e2e3cab1e693
github username <github email address>
2017-10-09 15:04:31 -0400

    added root route to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "movement_mixer_secret"
   end
-  
-end
+
+  get '/' do # root route is GET request to localhost:9393, the homepage
+    erb :index # render the index.erb view file, which is found in the views/ folder
+  end
