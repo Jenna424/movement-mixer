cd5ffaf2739f260f9eb19dcf5d134ffcb0f6152c
github username <github email address>
2017-10-09 19:15:29 -0400

    add create action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user' # render create_user.erb view file found in users/ subfolder within views/ folder
+    else
+      redirect to '/routines'
+    end
+  end
 end
