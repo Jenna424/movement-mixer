42db9062439ac4abdace8bc389e5018ee9043e8c
github username <github email address>
2017-10-09 21:00:17 -0400

    add validations for username, email, password attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_many :routines # a user instance has many routine instances (personalized workout plans)
+
+  has_secure_password
+
+  validates :username, :presence => true
+  # user instance must have @username attribute to be successfully saved to DB
+  validates :email, :presence => true
+  # user instance must have @email attribute to be successfully saved to DB
 end
