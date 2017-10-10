0c12c9020785b30aba123e0c3cf801ced3157c95
github username <github email address>
2017-10-10 11:28:43 -0400

    use @user instance variable and render signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   get '/signup' do # route is GET request to localhost:9393/signup, where form is presented to create a new user
     if !logged_in? # If a user is NOT already logged in,
       erb :'users/create_user' # render create_user.erb view file found in users/ subfolder within views/ folder
@@ -8,13 +9,13 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do # route receives data submitted in form to create new user
-    user = User.new(params) # instantiate user instance with its attributes set via mass assignment
+    @user = User.new(params) # instantiate user instance with its attributes set via mass assignment
     # user instance is successfully saved to DB if ALL form fields for username, email and password are filled out
-    if user.save
-      session[:user_id] = user.id # log in the newly created user
+    if @user.save
+      session[:user_id] = @user.id # log in the newly created user
       redirect to '/routines' # newly created, logged-in user sees index page of routines designed by all users
-    else # otherwise, if the user left username, email or password fields blank (value is empty string)
-      redirect to '/signup' # redirect to registration page to try signing up again
+    else # otherwise, if the user left username, email or password field blank (value is empty string)
+      erb :'users/create_user' # render create_user.erb view file, which is found in the users/ subfolder in views/ folder to try registering again
     end
   end
 
