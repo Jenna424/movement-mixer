49628a97011a0a4110c4b127a28095a924f5d009
github username <github email address>
2017-10-10 19:49:23 -0400

    add validations for name and reps attributes of movement

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,4 +1,9 @@
 class Movement < ActiveRecord::Base
   has_many :movement_routines
   has_many :routines, through: :movement_routines
+
+  validates :name, :presence => true
+  # a movement instance must have @name attribute value to be successfully saved to DB
+  validates :reps, :presence => true
+  # a movement instance must have @reps attribute value to be successfully saved to DB
 end
