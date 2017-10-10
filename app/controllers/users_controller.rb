04c69897c5cea6e6f164ab93408adefca0bd7174
github username <github email address>
2017-10-09 21:44:42 -0400

    defined post '/signup' route to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
       redirect to '/routines' # user is redirected to localhost:9393/routines, where all routines are displayed
     end
   end
+
+  post '/signup' do # route receives data submitted in form to create new user
+    # if the user left username, email or password fields blank (value is empty string)
+    if params.values.any?{|v| v == ""} # calling #values on params hash returns array of hash values. Calling #any? returns true if any element in array is empty string
+      redirect to '/signup' # redirect to registration page to try signing up again
+    else
+      @user = User.create(params) # instantiate user instance with attributes set via mass assignment
+      session[:user_id] = @user.id # log in the new user
+      erb :'users/show' # show action to display user just created - render the show.erb view file found within the users/ subfolder in the views/ folder
+    end
+  end
+
 end
