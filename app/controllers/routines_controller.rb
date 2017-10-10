424d02991ba5f8074f6f16cd7ab574e6b53a36e8
github username <github email address>
2017-10-10 15:30:30 -0400

    fix flash message in index action and define create action for new routine

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -8,8 +8,17 @@ class RoutinesController < ApplicationController
       @all_routines = Routine.all # @all_routines stores array of all routine instances
       erb :'routines/index' # render the index.erb view file, found within the routines/ subfolder in the views/ folder
     else # Otherwise, if the user is NOT logged in
-      flash[:message] = "You must be logged in to view the index of all workout routines."
+      flash[:message] = "You must log in to view the index of all workout routines."
       redirect to "/login" # redirect to localhost:9393/login, which presents login form
+    end
+  end
+
+  get '/routines/new' do # route is GET request to localhost:9393/routines/new to allow a logged in user to create a new workout routine
+    if logged_in? # a user must be logged in to create a new workout routine
+      erb :'routines/create_routine' # render the create_routine.erb view file, found in the routines/ subfolder within the views/ folder
+    else
+      flash[:message] = "You must log in to design a personalized workout plan."
+      redirect to '/login'
     end
   end
 
