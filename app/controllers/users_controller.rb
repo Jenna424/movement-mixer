4db8e2f08b0ea6cb4b266a9f5a631af546117595
github username <github email address>
2017-10-10 12:00:09 -0400

    create route to render login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,16 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id # log in the newly created user
       redirect to '/routines' # newly created, logged-in user sees index page of routines designed by all users
-    else # otherwise, if the user left username, email or password field blank (value is empty string)
+    else # otherwise, if the user left username, email or password form field blank (value is empty string)
       erb :'users/create_user' # render create_user.erb view file, which is found in the users/ subfolder in views/ folder to try registering again
+    end
+  end
+
+  get '/login' do # route is GET request to localhost:9393/login, where user sees login form
+    if logged_in? # a user who is already logged in will see the routines index page instead of login form
+      redirect to '/routines'
+    else # otherwise, if the user is NOT already logged in, they should see the login form
+      erb :'users/login' # render the login.erb view file, found within the users/ subfolder within the views/ folder
     end
   end
 
