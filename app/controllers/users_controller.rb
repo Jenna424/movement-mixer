eb30992f31b7d966cba974e0c289de1a90ca8cd4
github username <github email address>
2017-10-09 20:06:38 -0400

    add pseudo code explanation to signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  get '/signup' do
-    if !logged_in?
+  get '/signup' do # route is GET request to localhost:9393/signup, where form is presented to create a new user
+    if !logged_in? # If a user is NOT already logged in,
       erb :'users/create_user' # render create_user.erb view file found in users/ subfolder within views/ folder
-    else
-      redirect to '/routines'
+    else # otherwise, if the user is already logged in, they will not see signup form and instead,
+      redirect to '/routines' # user is redirected to localhost:9393/routines, where all routines are displayed
     end
   end
 end
