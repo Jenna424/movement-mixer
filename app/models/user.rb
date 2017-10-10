47b40294490f7af028f65beb8911b8eed89e3e6e
github username <github email address>
2017-10-09 21:03:13 -0400

    comment out explanation of has_secure_password macro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_many :routines # a user instance has many routine instances (personalized workout plans)
 
-  has_secure_password
-
+  has_secure_password # user instance has @password attribute even though table column is password_digest
+# user instance must have @password attribute to be successfully saved to DB
   validates :username, :presence => true
   # user instance must have @username attribute to be successfully saved to DB
   validates :email, :presence => true
