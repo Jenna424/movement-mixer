fe502678d3890cecc4bc84d4d40df61ea62e39fd
github username <github email address>
2017-10-09 21:12:31 -0400

    renamed password column in users table password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009211014) do
+ActiveRecord::Schema.define(version: 20171010011025) do
 
   create_table "movement_routines", force: :cascade do |t|
     t.integer "routine_id"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20171009211014) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
