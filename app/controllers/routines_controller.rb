0d54b2e243c827e7becad7786ab3e0723e2b825e
github username <github email address>
2017-10-10 15:16:49 -0400

    load and mount rack-flash, add conditional logic to routines index action

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,7 +1,15 @@
-class RoutinesController < ApplicationController
-  get '/routines' do # route is GET request to localhost:9393/routines, the index page to display all workout routines designed by all users
-    @all_routines = Routine.all # @all_routines stores array of all routine instances
+require 'rack-flash'
 
-    erb :'routines/index' # render the index.erb view file, found within the routines/ subfolder in the views/ folder
+class RoutinesController < ApplicationController
+  use Rack::Flash
+
+  get '/routines' do # route is GET request to localhost:9393/routines, the index page to display all workout routines designed by all users
+    if logged_in? # user can only see index of all workout routines if user is logged in
+      @all_routines = Routine.all # @all_routines stores array of all routine instances
+      erb :'routines/index' # render the index.erb view file, found within the routines/ subfolder in the views/ folder
+    else # Otherwise, if the user is NOT logged in
+      redirect to "/login" # redirect to localhost:9393/login, which presents login form
+    end
   end
+
 end
