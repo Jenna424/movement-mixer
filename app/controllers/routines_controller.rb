b74be3a4b13cf7991141580db2b51ee010832e54
github username <github email address>
2017-10-10 15:22:23 -0400

    add flash message to get '/routines' route

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -8,6 +8,7 @@ class RoutinesController < ApplicationController
       @all_routines = Routine.all # @all_routines stores array of all routine instances
       erb :'routines/index' # render the index.erb view file, found within the routines/ subfolder in the views/ folder
     else # Otherwise, if the user is NOT logged in
+      flash[:message] = "You must be logged in to view the index of all workout routines."
       redirect to "/login" # redirect to localhost:9393/login, which presents login form
     end
   end
