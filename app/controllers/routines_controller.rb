757ccb9315de3a60b1b74e268a573eeaa56829a3
github username <github email address>
2017-10-11 15:24:37 -0400

    add show action to display a single routine

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -22,4 +22,34 @@ class RoutinesController < ApplicationController
     end
   end
 
+  post '/routines' do # route receives data submitted in form to create a new routine.
+    @routine = Routine.new(params[:routine])
+    # The argument passed to #new is the routine hash (nested inside params hash)
+    # routine hash looks like: {"name" => "@name value", "training_type" => "@training_type value", "duration" => "@duration value", "difficulty_level" => "@difficulty_level value", "equipment" => "@equipment value", "movement_ids" => [array of string @id values of existing movement instances belonging to routine instance]}
+    # Instantiate @routine instance with its attribute values set via mass assignment, and
+    # Now we can also call @routine.movements and the array of existing movement instances belonging to the @routine instance is returned
+    if @routine.save # a routine instance will only be saved to DB if user filled in Name, Training Type, Duration, Difficulty Level and Equipment required form fields
+      if params[:movement].values.detect {|value| value == ""}
+        flash[:message] = "You must fill in Name, Instructions, Target Area, Reps, Modification and Challenge fields to create a new exercise movement for your workout routine."
+        redirect to "/routines/new" # present the form to try creating a new routine again
+      else # if user filled in all fields to create a new movement belonging to the new routine
+        @routine.movements << Movement.create(params[:movement])
+        # argument passed to #create is movement hash nested inside params hash
+        # movement hash looks like: {"name" => "@name", "instructions" => "@instructions", "target_area" => "@target_area", "reps" => "@reps", "modification" => "@modification", "challenge" => "@challenge"}
+        # instantiate and save to DB a movement instance with its attribute values set via mass assignment and
+        # shovel this new movement instance into the array of movement instances belonging to the @routine instance
+        flash[:message] = "You've successfully created a new workout routine!"
+        redirect to "/routines/#{@routine.generate_slug}" # show the user the workout routine they just created
+      end
+    else # otherwise, the routine instance was not saved to DB because user forgot to fill in required form fields
+      flash[:message] = "You must fill in Name, Training Type, Duration, Difficulty Level and Equipment fields to create a new workout routine."
+      redirect to '/routines/new' # redirect to localhost:9393/routines/new, where user sees form to try creating new routine again
+    end
+  end
+
+  get '/routines/:slug' do # route is GET request to localhost:9393/routines/slugged-version-of-@name-attribute-value-of-routine-instance-goes-here
+    @routine = Routine.find_by_slugged_name(params[:slug])
+    erb :'routines/show' # render the show.erb view file found within the routines/ subfolder in the views/ folder
+  end
+
 end
