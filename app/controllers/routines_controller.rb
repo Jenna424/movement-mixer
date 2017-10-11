dfd7f9ee68cba65e0150f388ed6770b753317eee
github username <github email address>
2017-10-11 19:13:59 -0400

    change view rendered by index action to routines_index.erb

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -6,7 +6,7 @@ class RoutinesController < ApplicationController
   get '/routines' do # route is GET request to localhost:9393/routines, the index page to display all workout routines designed by all users
     if logged_in? # user can only see index of all workout routines if user is logged in
       @all_routines = Routine.all # @all_routines stores array of all routine instances
-      erb :'routines/index' # render the index.erb view file, found within the routines/ subfolder in the views/ folder
+      erb :'routines/routines_index' # render the routines_index.erb view file, found within the routines/ subfolder in the views/ folder
     else # Otherwise, if the user is NOT logged in
       flash[:message] = "You must log in to view the index of all workout routines."
       redirect to "/login" # redirect to localhost:9393/login, which presents login form
