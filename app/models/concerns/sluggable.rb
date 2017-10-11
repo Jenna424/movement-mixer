c62bfbe7a5e0614fd9b10554fb475898f83ddfbd
github username <github email address>
2017-10-11 13:08:24 -0400

    add nested ClassMethods module, define #find_by_slugged_name

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -1,7 +1,15 @@
 module Sluggable
+
   module InstanceMethods
     def generate_slug # returns slugged version of instance's @name attribute value
       name.downcase.gsub(" ", "-") # call #name instance getter on instance (self) to return its @name attribute and then
     end # make @name value all lowercase and replace any spaces with hyphens
   end
+
+  module ClassMethods
+    def find_by_slugged_name(slugged_name)
+      all.detect {|instance| instance.generate_slug == slugged_name} # call #all on class to return array of all instances of that class
+    end # Iterate over array of all instances and return the first instance whose slugged version of its @name attribute (returned by calling #generate_slug on instance) equals slugged_name argument
+  end
+  
 end
