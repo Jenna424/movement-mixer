a0b6c24b9aa8263f3a8ed131790624223cae4c60
github username <github email address>
2017-10-11 12:59:17 -0400

    add nested InstanceMethods module, define #generate_slug

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -1,2 +1,7 @@
 module Sluggable
+  module InstanceMethods
+    def generate_slug # returns slugged version of instance's @name attribute value
+      name.downcase.gsub(" ", "-") # call #name instance getter on instance (self) to return its @name attribute and then
+    end # make @name value all lowercase and replace any spaces with hyphens
+  end
 end
