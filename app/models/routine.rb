b8c4bdbf640ccd09220b421debcd03402cfe379a
github username <github email address>
2017-10-11 12:17:46 -0400

    validate name attribute instead of title

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,10 +2,11 @@ class Routine < ActiveRecord::Base
   belongs_to :user # a routine instance belongs to a single user instance
   has_many :movement_routines
   has_many :movements, through: :movement_routines
-# routine instance must have @title, @training_type, @duration, @difficulty_level and @equipment attributes set to be successfully saved to DB
-  validates :title, :presence => true
+# routine instance must have @name, @training_type, @duration, @difficulty_level and @equipment attributes set to be successfully saved to DB
+  validates :name, :presence => true
   validates :training_type, :presence => true
   validates :duration, :presence => true
   validates :difficulty_level, :presence => true
   validates :equipment, :presence => true
+
 end
