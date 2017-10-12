16348f4e765013e89dec73c5ff12f8a976f63362
github username <github email address>
2017-10-12 18:45:13 -0400

    change username to name attribute in explanation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   include Sluggable::InstanceMethods
   extend Sluggable::ClassMethods
-  
+
   has_secure_password # user instance has @password attribute even though column in users table is password_digest
   # user instance must have @password attribute to be successfully saved to DB
   validates :name, :presence => true
-  # user instance must have @username attribute to be successfully saved to DB
+  # user instance must have @name attribute to be successfully saved to DB
   validates :email, :presence => true
   # user instance must have @email attribute to be successfully saved to DB
   has_many :routines # a user instance has many routine instances (personalized workout plans)
