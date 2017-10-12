4b236cf499f718334cc53dfcc125080f02358cc4
github username <github email address>
2017-10-12 15:02:47 -0400

    change movement index view to movements_index, add create action

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -2,6 +2,15 @@ class MovementsController < ApplicationController
   get '/movements' do # route is GET request to localhost:9393/movements to display index of all movements included in all workouts
     @all_movements = Movement.all
 
-    erb :'movements/index' # render the index.erb view file, which is found in the movements/ subfolder within the views/ folder
+    erb :'movements/movements_index' # render the movements_index.erb view file, which is found in the movements/ subfolder within the views/ folder
   end
+
+  get '/movements/new' do
+    if logged_in?
+      erb :'movements/create_movement'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
