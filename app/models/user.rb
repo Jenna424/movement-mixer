1d52b0f8ae9add01cfcb54e8d5d8d3ab5f595017
github username <github email address>
2017-10-16 10:44:22 -0400

    add has many through macro to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   # user instance must have @email attribute to be successfully saved to DB
   has_many :routines # a user instance has many routine instances (personalized workout plans)
+  # since a user instance has many routine instances, and each routine instance has many movement instances,
+  has_many :movements, through: :routines # a user instance has many movement instances through the routine instances that belong to that user
 end
