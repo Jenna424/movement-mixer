68c48687453aad67c7f586927851cfe53123abfd
github username <github email address>
2017-10-17 14:28:39 -0400

    add index action to display all exercise movements

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -2,4 +2,14 @@ require 'rack-flash'
 
 class MovementsController < ApplicationController
   use Rack::Flash
+
+  get '/movements' do # index action - route is GET request to localhost:9393/movements to display all movements included in all workout routines
+    if logged_in? # the user can only see the index of all exercise movements if logged in
+      @all_movements = Movement.all # @all_movements is an array storing all movement instances
+      erb :'movements/movements_index' # render the movements_index.erb view file, which is found in the movements/ subfolder within the views/ folder
+    else
+      redirect to '/login'
+    end
+  end
+
 end
