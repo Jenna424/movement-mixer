8c5874241bfcfc531ab675da0c1cc59123158fef
github username <github email address>
2017-10-17 16:12:08 -0400

    add conditional logic in post '/movements' route

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -19,4 +19,12 @@ class MovementsController < ApplicationController
       redirect to '/login'
     end
   end
+
+  post '/movements' do # route receives data submitted in form to create new exercise movement
+    if params[:movement].values.any? {|value| value.empty?} # if the user forgot to fill in any required field for movement attribute
+      flash[:message] = "You must fill in Name, Instructions, Target Area, Reps, Modification and Challenge fields to create a new exercise movement."
+      redirect to "/movements/new"
+    end
+  end
+
 end
