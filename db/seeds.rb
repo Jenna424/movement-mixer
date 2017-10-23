ef75d4be5e29211200f77cf308214a2ae903a264
github username <github email address>
2017-10-23 13:54:55 -0400

    edit seed data, create user and two movements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
 # I added seed data here. Seed database with 'rake db:seed'
-squat = Movement.new(name: "bodyweight squat", reps: 15)
-squat.instructions = "Stand with feet slightly wider than shoulder-width apart. While maintaining upright torso, bend knees and descend as if sitting down in a chair. Slowly ascend while gradually straightening legs."
-squat.target_area = "glutes, hamstrings and quadriceps"
-squat.modification = "Start with a shallow movement or use a wall to provide stability."
-squat.challenge = "Add a plyometric jump or a side-step."
-squat.save
+jenna = User.create(name: "Jenna", email: "Jenna424", password: "love2code")
 
-lunge = Movement.new(name: "forward lunge", reps: 10)
-lunge.instructions = "Maintaining upright posture, take a giant step forward with one leg. Descend until both knees are bent at a 90 degree angle, with your back knee hovering above the ground. Push yourself upwards to starting position."
-lunge.target_area = "glutes, hamstrings and quadriceps"
-lunge.modification = "Reduce range of motion by taking smaller steps."
-lunge.challenge = "Perform a bicep curl using dumbbells while lunging."
-lunge.save
+pushup_data = {
+  "name" => "Push-Up",
+  "instructions" => "Start on all fours in a high plank position, with your hands placed slightly wider than shoulder-width apart and your heels hovering just above the ground. Keeping your back flat and level, bend your elbows as you lower yourself down with your arms pressed close to your sides. Create your own resistance as you slowly rise back up, until your arms are fully straightened at the top of the range of motion. Repeat.",
+  "target_area" => "Triceps and shoulders",
+  "reps" => 10,
+  "sets" => 3,
+  "modification" => "Perform push-up in kneeling position",
+  "challenge" => "Pause at bottom of push-up and hold this position for 10 seconds"
+}
+
+squat_data = {
+  "name" => "Bodyweight Squat",
+  "instructions" => "Stand with your feet hip-width apart, maintaining upright posture. Bend your knees and descend as if you're sitting down in a chair. Slowly ascend while gradually straightening your legs.",
+  "target_area" => "Glutes, quads and hamstrings",
+  "reps" => 5,
+  "sets" => 3,
+  "modification" => "Use a smaller range of motion and don't squat as deeply",
+  "challenge" => "Add a plyometric jump between squats."
+}
+
+jenna.movements.create(pushup_data)
+jenna.movements.create(squat_data)
