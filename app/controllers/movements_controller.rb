fd4f70891c995032a6c6e6313aaa2f2d1b522d93
github username <github email address>
2017-10-31 07:43:01 -0400

    remove 'all workout routines' from index action comment

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -3,7 +3,7 @@ require 'rack-flash'
 class MovementsController < ApplicationController
   use Rack::Flash
 
-  get '/movements' do # index action - route is GET request to localhost:9393/movements to display all movements included in all workout routines
+  get '/movements' do # index action - route is GET request to localhost:9393/movements to display all movements
     if logged_in? # the user can only see the index of all exercise movements if logged in
       @all_movements = Movement.all # @all_movements is an array storing all movement instances
       erb :'movements/movements_index' # render the movements_index.erb view file, which is found in the movements/ subfolder within the views/ folder
